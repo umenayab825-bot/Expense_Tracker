db4e0fca9020961a19e2c752ccca675864e053fd
umenayab825 <umenayab825@gmail.com>
2025-12-12 19:54:33 +0500

    edit,delete,chart +background

---
+++ app/src/main/java/com/nayab/expensetracker/OnItemLongClickListener.java
@@ -0,0 +1,6 @@
+package com.nayab.expensetracker;
+
+// Interface that relays the Long Press event to MainActivity
+public interface OnItemLongClickListener {
+    void onItemLongClick(int transactionId);
+}
