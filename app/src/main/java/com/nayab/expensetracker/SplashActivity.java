550aa2b8b73e3345d74b339817b8845aa2086c69
umenayab825 <umenayab825@gmail.com>
2025-12-13 14:50:51 +0500

    splash screen

---
+++ app/src/main/java/com/nayab/expensetracker/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.nayab.expensetracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_DURATION);
+    }
+}
